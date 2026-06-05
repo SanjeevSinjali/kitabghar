8b19c3fb44783c83adc7136c4328918da86e321a
SanjeevSinjali <tdkzbiru@gmail.com>
2026-06-05 13:56:52 +0545

    Code Done

--- lib/app.dart
+++ lib/app.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:kitabghar/features/auth/presentation/pages/login_page.dart';
+import 'package:kitabghar/features/auth/presentation/pages/signup_page.dart';
+import 'package:kitabghar/features/dashboard/presentation/pages/dashboard_page.dart';
 import 'package:kitabghar/features/splash/presentation/pages/splashscreen.dart';
-
 
 class App extends StatelessWidget {
   const App({super.key});
@@ -10,7 +12,12 @@ class App extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       theme: ThemeData(primarySwatch: Colors.indigo, useMaterial3: true),
-      home: SplashView(),
+      home: const SplashView(),
+      routes: {
+        '/login': (_) => const LoginPage(),
+        '/signup': (_) => const SignupPage(),
+        '/dashboard': (_) => const DashboardPage(),
+      },
     );
   }
-} 
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,21 @@
-import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:kitabghar/app.dart';
+import 'package:kitabghar/core/services/hive/hive_service.dart';
+import 'package:kitabghar/features/auth/presentation/view_model/auth_view_model.dart';
 
-void main(){
-  runApp(const App());
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  final hiveService = HiveService();
+  await hiveService.init();
+
+  runApp(
+    ProviderScope(
+      overrides: [
+        hiveServiceProvider.overrideWithValue(hiveService),
+      ],
+      child: const App(),
+    ),
+  );
 }
